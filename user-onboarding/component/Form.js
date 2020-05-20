8b63869eabfa4fcf9a860dcff5b0d76c27439cc0
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-05-20 15:26:53 -0500

    form pseudo coded and imported to App

--- user-onboarding/component/Form.js
+++ user-onboarding/component/Form.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 function Form() {
+    //create advanced form here
+    //Name, Email, password, Terms of Service (checkbox) and Submit button
+    //Name, Email, Password and TOS required before Submit is available
     return (
         <div>
             
