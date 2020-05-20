2cd6b5ccfd82ffe25ad1a29ecd076eee91eb6dd5
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-05-20 15:39:19 -0500

    form partial structure done

--- user-onboarding/component/Form.js
+++ user-onboarding/component/Form.js
@@ -5,9 +5,25 @@ function Form() {
     //Name, Email, password, Terms of Service (checkbox) and Submit button
     //Name, Email, Password and TOS required before Submit is available
     return (
-        <div>
+       <form>
+           <div className='form-text'>
+               <h2>Title Goes Here Maybe</h2>
+               {/* // Text Input // */}
+               <label>Name:&nbsp;</label>
+               <label>Email:&nbsp;</label>
+               <label>Password &nbsp;</label>
+
+            <div className='form-checkbox'>
+               {/* // Checkbox // */}
+               <label>Terms of Service</label>
+            </div>{/* // Ends form checkbox div*/}
             
-        </div>
+            <div className='submit-btn'>
+                <button className='btn'>Submit</button>
+            </div>{/* // Ends form submit-btn div*/}
+
+           </div>{/* // Ends form text div*/}
+       </form>
     )
 }
 
