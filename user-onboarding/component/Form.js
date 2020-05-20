b67b029d582eef39bb51bd53e5a6b2059e0ed2e8
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-05-20 16:13:08 -0500

    basic code for form done

--- user-onboarding/component/Form.js
+++ user-onboarding/component/Form.js
@@ -1,6 +1,15 @@
 import React from 'react'
+//form will have props passed into it
+function Form(props) {
 
-function Form() {
+    const { 
+        values, 
+        onInputChange, 
+        onSubmit, 
+        onCheckboxChange, 
+        disabled, 
+        errors, 
+    } = props;
     //create advanced form here
     //Name, Email, password, Terms of Service (checkbox) and Submit button
     //Name, Email, Password and TOS required before Submit is available
@@ -9,17 +18,53 @@ function Form() {
            <div className='form-text'>
                <h2>Title Goes Here Maybe</h2>
                {/* // Text Input // */}
-               <label>Name:&nbsp;</label>
-               <label>Email:&nbsp;</label>
-               <label>Password &nbsp;</label>
+               <label>
+                   Name:&nbsp;
+                   <input 
+                   value={values.name}
+                   onChange={onInputChange}
+                   name='name'
+                   type='text'
+                   />
+                </label>
+
+               <label>
+                   Email:&nbsp;
+                   <input 
+                   value={values.email}
+                   onChange={onInputChange}
+                   name='email'
+                   type='email'
+                   />
+                </label>
+
+               <label>
+                   Password &nbsp;
+                   <input 
+                   value={values.password}
+                   onChange={onInputChange}
+                   name='password'
+                   type='text'
+                   />
+                </label>
 
             <div className='form-checkbox'>
                {/* // Checkbox // */}
-               <label>Terms of Service</label>
+               <label>
+                   Terms of Service &nbsp;
+                   <input 
+                   type='checkbox'
+                   name='TOS'
+                   onChange={onCheckboxChange}
+                   checked={values}
+                   />
+                </label>
             </div>{/* // Ends form checkbox div*/}
             
             <div className='submit-btn'>
-                <button className='btn'>Submit</button>
+                <button className='btn' disabled={disabled}>
+                    Submit
+                </button>
             </div>{/* // Ends form submit-btn div*/}
 
            </div>{/* // Ends form text div*/}
