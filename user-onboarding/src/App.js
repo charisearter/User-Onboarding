c3ff91cb9cd0444efeb32de499d0c7d6b09bc01f
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-05-21 09:59:24 -0500

    done just styling still get the strange button is set to ON error

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,148 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import Form from './component/Form';
-import validForm from './component/validForm';
-import axios from 'axios';
-import * as yup from 'yup';
 
-// Setting initial form  values
 
-const initialFormValues = {
-  //Text inputs//
-  fname: '',
-  lname: '',
-  email: '',
-  password: '',
-
-  //checkbox input TOS
-  terms: ''
-}
-
-const initialFormErrors = {
-  fname: '',
-  lname: '',
-  email: '',
-  password: '',
-  terms: ''
-}
-
-//area for users
-
-const initialUsers = [];
-
- //disabled btn 
-const initialDisabled= true;
 
 
 function App() {
-//set state for Users
-  const [ users, setUsers ] = useState(initialUsers)
-//set state for forms
-const [ formValues, setFormValues ] = useState(initialFormValues)
-
-//set state for form errors
-const [ formErrors, setFormErrors ] = useState(initialFormErrors)
-
-//set state for disabled button
-const [ disabled, setDisabled ] = useState(initialDisabled)
-
-
-const postNewUser = newUser => {
-  axios.post('https://reqres.in/api/users', newUser)
-  .then(res => {
-    setUsers([res.data.data, ...users])
-    console.log(res.data.data)
-  })
-  .catch(err => {
-    debugger
-  })
-  .finally(() => {// reset back to start?
-    setFormValues(initialFormValues)
-  })
-}
-
-//Event handlers
-
-const onInputChange = e => {
-  
-    const { name } = e.target.name;
-    const { value } = e.target.value;
-    
-
-  //yup validation
-
-  yup
-  .reach(validForm, e.target.name)
-  .validate(e.target.value)
-  .then(valid => {
-    setFormErrors({
-      ...formErrors,
-      [e.target.name]: ''
-    })
-  })
-  .catch(err => {
-    setFormErrors({
-      ...formErrors,
-      [e.target.name]: err.errors[0]
-    })
-  })
-
-//Successful or not set state to the new value
-  setFormValues({ ...formValues, [e.target.name]: e.target.value })
-
-}
-
-// const onCheckboxChange = e => {
-//   const { name } = e.target //name is target name
-//   const { checked } = e.target //checked to see if target is checked off
-
-//   setFormValues({ //set new state for form
-//     ...formValues, //copy form values
-    
-//     //want to check and see if terms is checked
-//     terms: { //overide form value
-//       ...formValues.terms, //copy current terms
-//       [name]: checked,  // override
-//     }
-//   })
-// }
-
-  const onSubmit = evt => {
-    evt.preventDefault()
-
-    const newUser = {
-      fname: formValues.fname.trim(),
-      lname: formValues.lname.trim(),
-      email: formValues.email.trim(),
-      password: formValues.password.trim(),
-      terms: setDisabled(true)
-     
-    }
-
-    postNewUser(newUser)
-}
-
-//side effect
-useEffect(() => {
-  //  ADJUST THE STATUS OF `disabled` EVERY TIME `formValues` CHANGES
-  validForm.isValid(formValues)
-    .then(valid => {
-      setDisabled(!valid)
-    })
-}, [formValues])
 
   return (
-    <div>
-        
-      <Form 
-      values={formValues}
-      onInputChange={onInputChange}
-      onSubmit={onSubmit}
-      disabled={disabled}
-      errors={formErrors}
-      //onCheckboxChange={onCheckboxChange}
-      />
-
+    <div>   
+      <Form />
     </div>
   );
 }
--- user-onboarding/src/component/Form.js
+++ user-onboarding/src/component/Form.js
@@ -1,41 +1,130 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import * as yup from 'yup';
 //form will have props passed into it
-function Form(props) {
+const Form = ()  => {
+const [ formState, setFormState ] = useState({
+    fname: '',
+    lname: '',
+    email: '',
+    password: '',
+    terms: '',
+});
 
-    const { 
-        values, 
-        onInputChange, 
-        onSubmit, 
-        //onCheckboxChange, 
-        disabled, 
-        errors, 
-    } = props;
-    //create advanced form here
-    //Name, Email, password, Terms of Service (checkbox) and Submit button
-    //Name, Email, Password and TOS required before Submit is available
+const [ errors, setErrors ] = useState({
+    fname: '',
+    lname: '',
+    email: '',
+    password: '',
+    terms: '',
+});
+
+const [ btnDisabled, setBtnDisabled ] = useState('');
+
+const [ post, setPost ] = useState([]);
+
+const validForm = yup.object().shape({
+   
+        fname: yup.string()
+            .trim()
+            .min(2, 'The first name must be at least two characters long')
+            .required('The first name is a required field'),
+        lname: yup.string()
+            .trim()
+            .min(2, 'The last name must be at least two characters long')
+            .required('The last name is a required field'),
+        password: yup.string()
+            .trim()
+            .max(25, 'The password max character limit is twenty-five (25)')
+            .matches
+            (/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+            'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character')
+            .required('The password is a required field'),
+        email: yup.string()
+            .email('The email must be a valid email address')
+            .required('The email is a required field'),
+        terms: yup.boolean()
+            .oneOf([true], 'You must accept the Terms of Service')
+   
+});
+
+useEffect (() => {
+    console.log('the form state has changed');
+    validForm.isValid(formState).then((valid) => { setBtnDisabled(!valid); });
+});
+
+const validateChange = (e) => {
+    yup
+    .reach(validForm, e.target.name)
+    .validate(e.target.value)
+    .then(valid => {
+      setErrors({
+        ...errors,
+        [e.target.name]: ''
+      });
+    })
+    .catch(err => {
+      setErrors({
+        ...errors,
+        [e.target.name]: err.errors[0]
+      });
+    });
+};
+
+const inputChange = (e) => {
+    e.persist();
+
+    const newFormData = {
+        ...formState,
+        [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value
+    };
+    validateChange(e);
+    setFormState(newFormData);
+};
+
+const formSubmit = (e) => {
+    e.preventDefault();
+    axios
+    .post('https://reqres.in/api/users', formState)
+    .then((res) => {
+        setPost(res.data);
+        console.log('success', post);
+        setFormState({
+            fname: '',
+            lname: '',
+            email: '',
+            password: '',
+            terms: ''
+        });
+    })
+    .catch((err) => {
+        console.log(err.response);
+    });
+};
     return (
-       <form className='form-container' onSubmit={onSubmit}>
+       <form className='form-container' onSubmit={formSubmit}>
            <div className='form-text'>
-               <h2>Title Goes Here Maybe</h2>
+               <h2>Fill out this form, please.</h2>
 
                {/* // Text Input // */}
-               <label>
+               <label htmlFor='first-name'>
                    First Name:&nbsp;
                    <input 
-                   value={values.fname}
-                   onChange={onInputChange}
+                   value={formState.fname}
+                   onChange={inputChange}
+                   id='first-name'
                    name='fname'
                    type='text'
                    placeholder='Fluffy'
                    />
                 </label>
 
-                <label>
+                <label htmlFor='last-name'>
                    Last Name:&nbsp;
                    <input 
-                   value={values.lname}
-                   onChange={onInputChange}
-                   id='lname'
+                   value={formState.lname}
+                   onChange={inputChange}
+                   id='last-name'
                    name='lname'
                    type='text'
                    placeholder='McFlooferkins'
@@ -45,8 +134,8 @@ function Form(props) {
                <label htmlFor='emailInput'>
                    Email:&nbsp;
                    <input 
-                   value={values.email}
-                   onChange={onInputChange}
+                   value={formState.email}
+                   onChange={inputChange}
                    id='emailInput'
                    name='email'
                    type='email'
@@ -57,8 +146,8 @@ function Form(props) {
                <label htmlFor='passwordInput'>
                    Password: &nbsp;
                    <input 
-                   value={values.password}
-                   onChange={onInputChange}
+                   value={formState.password}
+                   onChange={inputChange}
                    id='passwordInput'
                    name='password'
                    type='password'
@@ -67,7 +156,7 @@ function Form(props) {
                 </label>
 
                 {/* // Errors // */}
-            <div className='errors'>
+            <div className='alert'>
                 <div> {errors.fname} </div>
                 <div> {errors.lname} </div>
                 <div> {errors.email} </div>
@@ -78,13 +167,13 @@ function Form(props) {
             <div className='form-checkbox'>
                
                <label htmlFor='termsInput'>
-                   <h4>Do you agree to the Terms of Service</h4> &nbsp;
+                   <h4>Do you agree to the Terms of Service?</h4> &nbsp;
                    <input 
                    type='checkbox'
                    name='terms'
                    id='termsInput'
-                   checked={false}
-                   onChange={onInputChange}
+                   checked={formState.terms}
+                   onChange={inputChange}
                    />
                 </label>
             </div>
@@ -92,7 +181,7 @@ function Form(props) {
              {/* // Submit Button // */}
             <div className='submit-btn'>
                 {/* //disable button until form is complete */}
-                <button className='btn' disabled={disabled}>
+                <button className='btn' disabled={btnDisabled} type='submit'>
                     Submit
                 </button>
             </div>{/* // Ends form submit-btn div*/}
